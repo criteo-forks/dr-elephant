46404a94bc2a3d3334dd3f4e5f1c6d76ea18128e
n.fraison <n.fraison@criteo.com>
2019-05-15 14:34:32 +0200

    Use EmbeddedMysql for Garmadon sql tests
    Tests are broekn since the use of inner join and table aliases
    as H2 doesn't manage those sql pattern
    In order to keep this optimize query and the test we are now using
    an embedded mysql server

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -99,7 +99,9 @@ object Dependencies {
     "org.jmockit" % "jmockit" % "1.23" % Test,
     "org.scala-lang.modules" %% "scala-async" % "0.9.5",
     "org.apache.httpcomponents" % "httpclient" % "4.5.2",
+    "com.wix" % "wix-embedded-mysql" % "4.2.0" % Test,
     "org.scalatest" %% "scalatest" % "3.0.0" % Test
+
 
   ) :+ sparkExclusion :+ sparkSql
 
